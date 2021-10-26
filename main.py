89909d4fe17585658b4beedb8f97a824e576dfb6
Anna <aelise17264@gmail.com>
2021-10-25 20:38:59 -0600

    added timer to flip the card to the English definition after a 3-second delay

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-# ------------------------------ FLIP CARD --------------------------------- #
-
 
 # ---------------------------- GET VOCAB WORD ------------------------------- #
 vocab_list = pandas.read_csv("data/arabic_words.csv")
@@ -13,19 +11,31 @@ to_learn = vocab_list.to_dict(orient="records")
 
 
 def vocab_word():
+    global current_card, timer
+    window.after_cancel(card_flip)
     current_card = random.choice(to_learn)
     card.itemconfig(card_title, text="Arabic")
     card.itemconfig(card_word, text=current_card["Arabic"])
+    card.itemconfig(card_background, image=front)
+    timer = window.after(3000, func=card_flip)
 
+# ------------------------------ FLIP CARD --------------------------------- #
+def card_flip():
+    card.itemconfig(card_title, text="English")
+    card.itemconfig(card_word, text=current_card["English"])
+    card.itemconfig(card_background, image=back)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 window.title("Flash Cards")
 
+timer = window.after(3000, func=card_flip)
+
 card = Canvas(width=800, height=526)
 front = PhotoImage(file="card_images/card_front.png")
-card.create_image(400, 263, image=front)
+back = PhotoImage(file="card_images/card_back.png")
+card_background = card.create_image(400, 263, image=front)
 card_title = card.create_text((400, 150), text="", font=("Arial", 40, "italic"))
 
 card_word = card.create_text((400, 263), text="", font=("Arial", 60, "bold"))
