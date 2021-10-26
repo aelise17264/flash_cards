7ec728375c11dd4bc51ef77f87883520d91d917f
Anna <aelise17264@gmail.com>
2021-10-25 21:24:23 -0600

    created function for user click the green check mark and create a new list of unknown words, this will keep track of user's progress

--- main.py
+++ main.py
@@ -4,26 +4,38 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-
 # ---------------------------- GET VOCAB WORD ------------------------------- #
-vocab_list = pandas.read_csv("data/arabic_words.csv")
-to_learn = vocab_list.to_dict(orient="records")
+
+try:
+    vocab_list = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_list = pandas.read_csv("data/arabic_words.csv")
+    to_learn = original_list.to_dict(orient="records")
+else:
+    to_learn = vocab_list.to_dict(orient="records")
 
 
 def vocab_word():
     global current_card, timer
-    window.after_cancel(card_flip)
+    window.after_cancel(timer)
     current_card = random.choice(to_learn)
-    card.itemconfig(card_title, text="Arabic")
-    card.itemconfig(card_word, text=current_card["Arabic"])
+    card.itemconfig(card_title, text="Arabic", fill="black")
+    card.itemconfig(card_word, text=current_card["Arabic"], fill="black")
     card.itemconfig(card_background, image=front)
     timer = window.after(3000, func=card_flip)
 
 # ------------------------------ FLIP CARD --------------------------------- #
 def card_flip():
-    card.itemconfig(card_title, text="English")
-    card.itemconfig(card_word, text=current_card["English"])
+    card.itemconfig(card_title, text="English", fill="white")
+    card.itemconfig(card_word, text=current_card["English"], fill="white")
     card.itemconfig(card_background, image=back)
+
+# ---------------------------- SAVING PROGRESS ------------------------------- #
+def right_answer():
+    to_learn.remove(current_card)
+    vocab_list = pandas.DataFrame(to_learn)
+    vocab_list.to_csv("data/words_to_learn.csv", index=False)
+    vocab_word()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -37,19 +49,18 @@ front = PhotoImage(file="card_images/card_front.png")
 back = PhotoImage(file="card_images/card_back.png")
 card_background = card.create_image(400, 263, image=front)
 card_title = card.create_text((400, 150), text="", font=("Arial", 40, "italic"))
-
 card_word = card.create_text((400, 263), text="", font=("Arial", 60, "bold"))
-
 card.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-card.grid(column=0, row=0, columnspan=2)
+card.grid(column=0, row=1, columnspan=2)
 
 right = PhotoImage(file="card_images/right1.png")
-right_button = Button(image=right, highlightthickness=0, command=vocab_word)
-right_button.grid(column=1, row=1)
+right_button = Button(image=right, highlightthickness=0, command=right_answer)
+right_button.config(pady=0)
+right_button.grid(column=1, row=2)
 
 wrong = PhotoImage(file="card_images/wrong.png")
 wrong_button = Button(image=wrong, highlightthickness=0, command=vocab_word)
-wrong_button.grid(column=0, row=1)
+wrong_button.grid(column=0, row=2)
 
 vocab_word()
 window.mainloop()
